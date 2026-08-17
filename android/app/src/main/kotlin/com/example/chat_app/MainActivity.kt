433aa9efd73e5b3d429c279ca7c3671b2b878b2d
Syed Muhammad Talha <syedmuhammadtalha800@gmail.com>
2026-08-17 23:48:45 +0500

    Firebase Added

--- android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/chat_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.chat_app
+package com.app.chatapp
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:chat_app/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
